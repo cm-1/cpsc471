e3badb4520f429f805ee3cb892c3224fc8718da0
C M <christopher.mossman@ucalgary.ca>
2018-12-02 14:11:58 -0700

    Created new folder for our actual app development.

---
+++ ClinicApp/clinicAppFiles/db.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var pool;
+
+function returnPool() {
+	var p = "ZakVezThok888!"; //Replace this with your personal root password
+	if(!p)
+		console.log("=========\n\nERROR: Hey guys! You gotta set the password in db.js! :-) \n\n=================");
+	if(!pool){
+		pool  = mysql.createPool({
+		  connectionLimit : 10,
+		  host            : 'localhost',
+		  user            : 'root',
+		  password        : p, 
+		  database        : 'clinicdb'
+		});
+	}
+	return pool;
+}
+
+module.exports = returnPool();
