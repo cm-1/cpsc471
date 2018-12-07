a87e3bed81c9b3286f5fe13c547beb1cd33c75a2
C M <christopher.mossman@ucalgary.ca>
2018-12-07 03:33:29 -0700

    Adding patients works.

--- ClinicApp/clinicAppFiles/routes/myinfo.js
+++ ClinicApp/clinicAppFiles/routes/myinfo.js
@@ -51,30 +51,56 @@ router.post('/info', function(req, res){
 	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);*/
 	
 	var query = "INSERT INTO contact_info (Title, Fname, Lname, Address, Email, Home_phone, Cell_phone) VALUES ?";
-	var values = [[username, email, hashedPassword]];
-	db.query(query, [values], function (err, result) {
+	var values_ci = [[title, fname, lname, address, email, hphone, cphone]];
+	db.query(query, [values_ci], function (err, result) {
 		if (err){
-			if (err.code == 'ER_DUP_ENTRY'){
-				var msg = "Error: Username '" + username + "' is already in use!";
-				if(err.message.indexOf("for key 'Email_UNIQUE'") > 0){
-					msg = "Error: Email address '" + email + "' is already in use!";
-				}
-				console.log("Well, there's a duplicate!");
-				req.flash('danger', msg);
-				res.redirect('/users/register');
-			}
-			else{
+			if (err){
 				console.log(err);
-			throw err;
+				throw err;
 			}
 			
 		} else{
-			console.log("# records inserted: " + result.affectedRows);
-
-			req.flash('success', 'Account Created!');
-
-			res.redirect('/users/login');
+			const ci_id = result.insertId;
 			
+			var query2 = "INSERT INTO Patient (Health_care_id, Sex, Birthdate, CI_ID) VALUES ?";
+			var values_p = [[hcid, sex, birthdate, ci_id]];
+			db.query(query2, [values_p], function (err, result) {
+				if (err) console.log(err);
+				if (err && err.code == 'ER_DUP_ENTRY'){
+					var msg = "Error: HCID '" + hcid + "' is already in use!";
+					console.log("Well, there's a duplicate!");
+					req.flash('danger', msg);
+					res.redirect('/myinfo/info');
+				}
+				else if (err){
+					console.log(err);
+					throw err;
+				}
+				else{
+					//var query3 = "SELECT COUNT(*) FROM client_account where id = "+req.user.id;
+					db.query("select count(*) from client_account where accountID =" + req.user.id, function (err, rows, fields){
+						console.log("\n\nCOUNT*!!!:");
+						console.log(rows);
+						if (err){
+							console.log(err);
+							throw err;
+						} 
+						if (rows[0]['count(*)'] <= 0) {
+							var query4 = "Insert into client_account (accountID, healthcare_id) values ?";
+							var values_ca = [[req.user.id, hcid]];
+							db.query(query4, [values_ca], function(err, results){
+								if(err) throw err;
+								req.flash("success", "Info updated!");
+								res.redirect('/');
+							});
+						} else{
+							req.flash('success', "Info updated!")
+							res.redirect('/');
+						}
+					});
+				}
+			});
+		
 		}
 	});
 
